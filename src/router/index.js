04d8ac8d1c79f8e1e74ea27abb7f6d845621c061
poper <poper@wangxutech.com>
2022-02-13 18:25:56 +0800

    底部导航完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,50 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login/login'
+import Layout from '@/views/layout/layout'
+import home from '@/views/layout/home/home'
+import qa from '@/views/layout/qa/qa'
+import video from '@/views/layout/video/video'
+import mine from '@/views/layout/mine/mine'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    redirect: '/login'
+    redirect: '/layout'
   },
   {
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/layout',
+    name: 'layout',
+    component: Layout,
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: home
+      },
+      {
+        path: 'qa',
+        name: 'qa',
+        component: qa
+      },
+      {
+        path: 'video',
+        name: 'video',
+        component: video
+      },
+      {
+        path: 'mine',
+        name: 'mine',
+        component: mine
+      }
+    ]
   }
 ]
 
---
+++ src/views/layout/layout.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="layout-container">
+    <router-view></router-view>
+
+    <div class="nav-bar">
+      <van-tabbar class="layout-nav-bar" route>
+        <van-tabbar-item replace to="/layout/home">
+          <span class="text">首页</span>
+          <template #icon>
+            <i class="iconfont icon-shouye"></i>
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item icon="search" replace to="/layout/qa">
+          <span>问答</span>
+          <template #icon>
+            <i class="iconfont icon-wenda"></i>
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item icon="friends-o" replace to="/layout/video">
+          <span>视频</span>
+          <template #icon>
+            <i class="iconfont icon-shipin"></i>
+          </template>
+        </van-tabbar-item>
+        <van-tabbar-item icon="setting-o" replace to="/layout/mine">
+          <span>我的</span>
+          <template #icon>
+            <i class="iconfont icon-wode"></i>
+          </template>
+        </van-tabbar-item>
+      </van-tabbar>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'layout',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="less">
+.nav-bar {
+  .layout-nav-bar {
+    i.iconfont {
+      font-size: 40px;
+    }
+
+    span.text {
+      font-size: 20px;
+    }
+  }
+}
+
+</style>
