e3c9c8150135da8176be5d5e849cbeeac1c354ff
poper <poper@wangxutech.com>
2022-02-19 00:18:28 +0800

    搜索页面配置完成

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="search-container">
-    搜索
+    <form action="/">
+      <van-search
+        v-model="value"
+        show-action
+        placeholder="请输入搜索关键词"
+        @search="onSearch"
+        @cancel="onCancel"
+        background="#3296fa"
+      />
+    </form>
   </div>
 </template>
 
 <script>
+import { Toast } from 'vant'
+
 export default {
-  name: 'search'
+  name: 'search',
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onSearch (val) {
+      Toast(val)
+    },
+    onCancel () {
+      Toast('取消')
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.search-container {
+  .van-search__action {
+    color: #ffffff;
+  }
+}
 </style>
